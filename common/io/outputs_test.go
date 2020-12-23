3229c698d00a58088b3a0f691e72ff1bf26df169
Tamás Benke <tombenke@gmail.com>
2020-12-23 12:38:05 +0100

    Refactor messages

--- common/io/outputs_test.go
+++ common/io/outputs_test.go
@@ -1,13 +1,13 @@
 package io
 
 import (
-	"github.com/tombenke/axon-go/common/msgs/std_msgs"
+	"github.com/tombenke/axon-go/common/msgs/base"
 	"testing"
 )
 
 func TestOutputsSetOutputMessage(t *testing.T) {
-	bmsg := std_msgs.NewBoolMessage(true)
-	o := Outputs{"State": Output{IO{Name: "State", Type: std_msgs.BoolTypeName, Message: bmsg}}}
-	(o).SetOutputMessage("State", &std_msgs.Bool{true})
+	bmsg := base.NewBoolMessage(true)
+	o := Outputs{"State": Output{IO{Name: "State", Type: base.BoolTypeName, Message: bmsg}}}
+	(o).SetOutputMessage("State", &base.Bool{true})
 	//TODO: Write the test
 }
